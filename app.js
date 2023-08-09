dbd85cb902030aad9c4bff6e496b63e2bdf859a1
时简 <55093271+mifengdeyanjing@users.noreply.github.com>
2023-08-09 11:22:36 +0800

    Update app.js
    这是我app.js的全部内容

--- app.js
+++ app.js
@@ -1,12 +1,57 @@
+// const express = require('express')
+// const test = require('./test/test')
+// const app = express()
+// const port = 3000
+
+// app.get('/', (req, res) => {
+//   res.send(test)
+// })
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
+
 const express = require('express')
-const test = require('./test/test')
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
 const app = express()
-const port = 3000
+const port = 5250
 
-app.get('/', (req, res) => {
-  res.send(test)
-})
 
+app.use(express.static('dist'))
+
+app.use('/pcrf',
+  createProxyMiddleware({
+    target: 'https://pcrf.hh-medic.com',
+    changeOrigin: true,
+  },)
+);
+app.use([
+  '/adminapi',
+  '/slebapi',
+  '/reportapi',
+  '/billapi',
+  '/tpa',
+],
+  createProxyMiddleware(
+    {
+      target: 'https://xadmin-test.hh-medic.com',
+      changeOrigin: true,
+    })
+);
+app.use([
+  '/familyapph5',
+  '/kcmenu',
+  '/familymadmin',
+  '/familyplatformbill',
+  '/grpapi',
+],
+  createProxyMiddleware(
+    {
+      target: 'https://xadmin-test.hh-medic.com',
+      changeOrigin: true,
+    })
+);
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
